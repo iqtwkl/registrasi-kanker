d7bc62aee87b2aba632367efe11e3bc56310903a
Zuliansyah <zd00595318@techMahindra.com>
2020-02-04 17:43:08 +0700

    Migration Database

---
+++ database/migrations/2020_02_04_171100_create_pasien_table.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TMPCZuliansyah
+ * Date: 04/02/2020
+ * Time: 17:12
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePasienTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pasien', function (Blueprint $table) {
+            $table->uuid("id");
+            $table->string("kode_rs");
+            $table->string("no_rekam_medis");
+            $table->integer("id_rs");
+            $table->string("nama_pasien");
+            $table->string("namaOrangTua");
+            $table->string("tempatLahir");
+            $table->date("tglLahir");
+            //addAttributeUsia
+            $table->string("pendidikan");
+            $table->string("pekerjaan");
+            $table->string("hamil");
+            $table->string("menyusui");
+            $table->string("rujuk");
+            $table->string("agama");
+            $table->string("perkawinan");
+            $table->string("anak");
+            $table->string("keturunan");
+            $table->string("suku");
+            $table->string("asing");
+            $table->string("daerah");
+            $table->string("jalan");
+            $table->string("rt");
+            $table->string("rw");
+            $table->string("no");
+            $table->string("kode_telp");
+            $table->string("telp");
+            $table->string("kelurahan");
+            $table->string("kecamatan");
+            $table->string("provinsi");
+            $table->string("kabupaten");
+            $table->string("jalan_o");
+            $table->string("rt_o");
+            $table->string("rw_o");
+            $table->string("no_o");
+            $table->string("kode_telp_o");
+            $table->string("telp_o");
+            $table->string("kelurahan_o");
+            $table->string("kecamatan_o");
+            $table->string("provinsi_o");
+            $table->string("kabupaten_o");
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pasien');
+    }
+}
---
+++ database/migrations/2020_02_04_172500_create_rumah_sakit_table.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TMPCZuliansyah
+ * Date: 04/02/2020
+ * Time: 17:26
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRumahSakitTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rumah_sakit', function (Blueprint $table) {
+            $table->uuid("id");
+            $table->string("kode");
+            $table->string("nama");
+            $table->text("alamat");
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rumah_sakit');
+    }
+}
